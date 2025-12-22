124e690682ae1c869e2e509ac58284956091ea74
Diana Lin <lindiana1206@gmail.com>
2025-12-22 01:15:21 -0500

    optimize most images

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,22 @@
 import { getCollection } from "astro:content";
+import type { ImageMetadata } from "astro";
+
+const images = import.meta.glob<ImageMetadata>(
+    "./assets/images/**/*.{jpg,jpeg,png,webp}",
+    { eager: true }
+);
+
+function getPicture(
+    src: string,
+): ImageMetadata | null {
+    const match = Object.entries(images).find(([path]) => path.endsWith(src));
+
+    if (match) {
+        return (match[1] as any).default;
+    }
+
+    return null;
+}
 
 const peopleCollection = await getCollection("people");
 
@@ -6,7 +24,7 @@ const getYearPictureSrc = (
     person: (typeof peopleCollection)[number],
     type: "professional" | "profile",
     year: number,
-): string | null => {
+): ImageMetadata | null => {
     const pictures =
         type === "professional"
             ? person.data.professionalPictures
@@ -15,7 +33,7 @@ const getYearPictureSrc = (
     if (pictures) {
         // Find pictureSrc for the given year
         const pictureForYear = pictures.find((pic) => pic.year === year);
-        if (pictureForYear) return pictureForYear.src;
+        if (pictureForYear) return getPicture(pictureForYear.src);
     }
 
     // Default to defaultProfilePicture
@@ -25,7 +43,7 @@ const getYearPictureSrc = (
         const src = person.data.professionalPictures?.find(
             (pic) => pic.year === defaultPic.year,
         )?.src;
-        if (src) return src;
+        if (src) return getPicture(src);
     }
 
     const defaultPic = person.data.defaultProfilePicture;
@@ -33,9 +51,9 @@ const getYearPictureSrc = (
     const src = person.data.profilePictures?.find(
         (pic) => pic.year === defaultPic.year,
     )?.src;
-    if (src) return src;
+    if (src) return getPicture(src);
 
     return null;
 };
 
-export { getYearPictureSrc };
+export { getPicture, getYearPictureSrc };
